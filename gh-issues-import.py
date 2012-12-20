20d3e7e84cc04ef2ff78db9fb0935238e51e3ba7
Andreas Renberg (IQAndreas) <iq_andreas@hotmail.com>
2012-12-20 08:50:02 +0100

    Remove repeated code for API requests
    Many lines of code were removed or reduced down to instead use these two functions:
     * send_post_request(url, data)
     * send_get_request(url)
    
    The benefits of this refactoring should be obvious.

--- gh-issues-import.py
+++ gh-issues-import.py
@@ -13,25 +13,32 @@ server = "api.github.com"
 src_url = "https://%s/repos/%s" % (server, src_repo)
 dst_url = "https://%s/repos/%s" % (server, dst_repo)
 
+def send_post_request(url, data):
+	req = urllib.request.Request(url, json.dumps(data))
+	req.add_header("Authorization", "Basic " + base64.urlsafe_b64encode("%s:%s" % (username, password)))
+	req.add_header("Content-Type", "application/json")
+	req.add_header("Accept", "application/json")
+	
+	response = urllib.request.urlopen(req)
+	json_data = response.read()
+	return json.loads(json_data.decode("utf-8"))
+
+def send_get_request(url):
+	response = urllib.request.urlopen(url)
+	json_data = response.read()
+	return json.loads(json_data.decode("utf-8"))
+
 def get_milestones(url):
-	response = urllib.request.urlopen("%s/milestones?state=open" % url)
-	result = response.read()
-	milestones = json.loads(result.decode("utf-8"))
-	return milestones
+	return send_get_request("%s/milestones?state=open" % url)
 
 def get_labels(url):
-	response = urllib.request.urlopen("%s/labels" % url)
-	result = response.read()
-	labels = json.loads(result.decode("utf-8"))
-	return labels
+	return send_get_request("%s/labels" % url)
 
 def get_issues(url):
 	issues = []
 	i = 1
 	while True:
-		response = urllib.request.urlopen("%s/issues?state=open&page=%d" % (url, i))
-		result = response.read()
-		newIssues = json.loads(result.decode("utf-8"))
+		newIssues = send_get_request("%s/issues?state=open&page=%d" % (url, i))
 		if not newIssues:
 			break
 		issues.extend(newIssues)
@@ -42,46 +49,30 @@ def get_comments_on_issue(issue):
 	if "comments" in issue \
 	  and issue["comments"] is not None \
 	  and issue["comments"] != 0:
-		response = urllib.request.urlopen("%s/comments" % issue["url"])
-		result = response.read()
-		comments = json.loads(result.decode("utf-8"))
-		return comments
+		return send_get_request("%s/comments" % issue["url"])
 	else :
 		return []
 
 def import_milestones(milestones):
 	for source in milestones:
-		dest = json.dumps({
+		data = {
 			"title": source["title"],
 			"state": "open",
 			"description": source["description"],
-			"due_on": source["due_on"]})
-
-		req = urllib.request.Request("%s/milestones" % dst_url, dest)
-		req.add_header("Authorization", "Basic " + base64.urlsafe_b64encode("%s:%s" % (username, password)))
-		req.add_header("Content-Type", "application/json")
-		req.add_header("Accept", "application/json")
-		res = urllib.request.urlopen(req)
-
-		data = res.read()
-		res_milestone = json.loads(data.decode("utf-8"))
+			"due_on": source["due_on"]
+		}
+		
+		res_milestone = send_post_request("%s/milestones" % dst_url, data)
 		print("Successfully created milestone %s" % res_milestone["title"])
 
 def import_labels(labels):
 	for source in labels:
-		dest = json.dumps({
+		data = {
 			"name": source["name"],
 			"color": source["color"]
-		})
-
-		req = urllib.request.Request("%s/labels" % dst_url, dest)
-		req.add_header("Authorization", "Basic " + base64.urlsafe_b64encode("%s:%s" % (username, password)))
-		req.add_header("Content-Type", "application/json")
-		req.add_header("Accept", "application/json")
-		res = urllib.request.urlopen(req)
-
-		data = res.read()
-		res_label = json.loads(data.decode("utf-8"))
+		}
+		
+		res_label = send_post_request("%s/labels" % dst_url, data)
 		print("Successfully created label %s" % res_label["name"])
 
 def import_comments(comments, issue_number):
@@ -89,22 +80,14 @@ def import_comments(comments, issue_number):
 		comment_creator = comment["user"]["login"]
 		comment["body"] = "Comment by: [%s](http://github.com/%s)\n\n%s" % (comment_creator, comment_creator, comment["body"])
 
-		req = urllib.request.Request("%s/issues/%s/comments" % (dst_url, issue_number), json.dumps(comment))
-		req.add_header("Authorization", b"Basic " + base64.urlsafe_b64encode(username.encode("utf-8") + b":" + password.encode("utf-8")))
-		req.add_header("Content-Type", "application/json")
-		req.add_header("Accept", "application/json")
-		urllib.request.urlopen(req)
+		send_post_request("%s/issues/%s/comments" % (dst_url, issue_number), comment)
 
 def import_pull_requests(issue, issue_number):
 	if "pull_request" in issue and issue["pull_request"]["html_url"] is not None:
 		committer_name = issue["user"]["login"]
-		comment = json.dumps({"body": "**[#%s](%s)** added a commit: %s" % (committer_name, committer_name, issue["pull_request"]["html_url"])})
+		comment = {"body": "**[#%s](%s)** added a commit: %s" % (committer_name, committer_name, issue["pull_request"]["html_url"])}
 
-		req = urllib.request.Request("%s/issues/%s/comments" % (dst_url, issue_number), comment)
-		req.add_header("Authorization", b"Basic " + base64.urlsafe_b64encode(username.encode("utf-8") + b":" + password.encode("utf-8")))
-		req.add_header("Content-Type", "application/json")
-		req.add_header("Accept", "application/json")
-		urllib.request.urlopen(req)
+		send_post_request("%s/issues/%s/comments" % (dst_url, issue_number), comment)
 
 def import_issues(issues, dst_milestones, dst_labels):
 	for issue in issues:
@@ -117,15 +100,7 @@ def import_issues(issues, dst_milestones, dst_labels):
 			issue_header = "Issue [#%s](%s) by: [%s](http://github.com/%s)\n" % (issue_id, issue_url, issue_creator, issue_creator)
 			issue["body"] = issue_header + "\n\n\n" + issue["body"]
 
-		req = urllib.request.Request("%s/issues" % dst_url, json.dumps(issue))
-		req.add_header("Authorization", b"Basic " + base64.urlsafe_b64encode(username.encode("utf-8") + b":" + password.encode("utf-8")))
-		req.add_header("Content-Type", "application/json")
-		req.add_header("Accept", "application/json")
-		res = urllib.request.urlopen(req)
-
-		data = res.read()
-		res_issue = json.loads(data.decode("utf-8"))
-
+		res_issue = send_post_request("%s/issues" % dst_url, issue)
 		import_pull_requests(issue, res_issue["number"])
 
 		comments = get_comments_on_issue(issue)
