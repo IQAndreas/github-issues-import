803b58e60666d72a0d02519a450869ae43598aef
moly <0700689@live.abertay.ac.uk>
2012-12-20 05:36:13 +0000

    Updated to python 3, added comment importing

--- gh-issues-import.py
+++ gh-issues-import.py
@@ -1,13 +1,12 @@
-import urllib2
+import urllib.request, urllib.error, urllib.parse
 import json
-from StringIO import StringIO
 import base64
 
 #==== configurations =======
-username = "user@example.com"
-password = "naked_password"
-src_repo = "octocat/Hello-World"
-dst_repo = "helloworld-inc/Hello-World"
+username = "username"
+password = "password"
+src_repo = "octocat/HelloWorld"
+dst_repo = "demo/HelloWorld"
 #==== end of configurations ===
 
 server = "api.github.com"
@@ -15,30 +14,37 @@ src_url = "https://%s/repos/%s" % (server, src_repo)
 dst_url = "https://%s/repos/%s" % (server, dst_repo)
 
 def get_milestones(url):
-	response = urllib2.urlopen("%s/milestones?state=open" % url)
+	response = urllib.request.urlopen("%s/milestones?state=open" % url)
 	result = response.read()
-	milestones = json.load(StringIO(result))
+	milestones = json.loads(result.decode("utf-8"))
 	return milestones
 
 def get_labels(url):
-	response = urllib2.urlopen("%s/labels" % url)
+	response = urllib.request.urlopen("%s/labels" % url)
 	result = response.read()
-	labels = json.load(StringIO(result))
+	labels = json.loads(result.decode("utf-8"))
 	return labels
 
 def get_issues(url):
-	response = urllib2.urlopen("%s/issues" % url)
-	result = response.read()
-	issues = json.load(StringIO(result))
+	issues = []
+	i = 1
+	while True:
+		response = urllib.request.urlopen("%s/issues?state=open&page=%d" % (url, i))
+		result = response.read()
+		newIssues = json.loads(result.decode("utf-8"))
+		if not newIssues:
+			break
+		issues.extend(newIssues)
+		i += 1
 	return issues
 
 def get_comments_on_issue(issue):
-	if issue.has_key("comments") \
+	if "comments" in issue \
 	  and issue["comments"] is not None \
 	  and issue["comments"] != 0:
-		response = urllib2.urlopen("%s/comments" % issue["url"])
+		response = urllib.request.urlopen("%s/comments" % issue["url"])
 		result = response.read()
-		comments = json.load(StringIO(result))
+		comments = json.loads(result.decode("utf-8"))
 		return comments
 	else :
 		return []
@@ -51,15 +57,15 @@ def import_milestones(milestones):
 			"description": source["description"],
 			"due_on": source["due_on"]})
 
-		req = urllib2.Request("%s/milestones" % dst_url, dest)
+		req = urllib.request.Request("%s/milestones" % dst_url, dest)
 		req.add_header("Authorization", "Basic " + base64.urlsafe_b64encode("%s:%s" % (username, password)))
 		req.add_header("Content-Type", "application/json")
 		req.add_header("Accept", "application/json")
-		res = urllib2.urlopen(req)
-		
+		res = urllib.request.urlopen(req)
+
 		data = res.read()
-		res_milestone = json.load(StringIO(data))
-		print "Successfully created milestone %s" % res_milestone["title"]
+		res_milestone = json.loads(data.decode("utf-8"))
+		print("Successfully created milestone %s" % res_milestone["title"])
 
 def import_labels(labels):
 	for source in labels:
@@ -68,63 +74,64 @@ def import_labels(labels):
 			"color": source["color"]
 		})
 
-		req = urllib2.Request("%s/labels" % dst_url, dest)
+		req = urllib.request.Request("%s/labels" % dst_url, dest)
 		req.add_header("Authorization", "Basic " + base64.urlsafe_b64encode("%s:%s" % (username, password)))
 		req.add_header("Content-Type", "application/json")
 		req.add_header("Accept", "application/json")
-		res = urllib2.urlopen(req)
+		res = urllib.request.urlopen(req)
 
 		data = res.read()
-		res_label = json.load(StringIO(data))
-		print "Successfully created label %s" % res_label["name"]
+		res_label = json.loads(data.decode("utf-8"))
+		print("Successfully created label %s" % res_label["name"])
+
+def import_comments(comments, issue_number):
+	for comment in comments:
+		comment_creator = comment["user"]["login"]
+		comment["body"] = "Comment by: [%s](http://github.com/%s)\n\n%s" % (comment_creator, comment_creator, comment["body"])
+
+		req = urllib.request.Request("%s/issues/%s/comments" % (dst_url, issue_number), json.dumps(comment))
+		req.add_header("Authorization", b"Basic " + base64.urlsafe_b64encode(username.encode("utf-8") + b":" + password.encode("utf-8")))
+		req.add_header("Content-Type", "application/json")
+		req.add_header("Accept", "application/json")
+		urllib.request.urlopen(req)
+
+def import_pull_requests(issue, issue_number):
+	if "pull_request" in issue and issue["pull_request"]["html_url"] is not None:
+		committer_name = issue["user"]["login"]
+		comment = json.dumps({"body": "**[#%s](%s)** added a commit: %s" % (committer_name, committer_name, issue["pull_request"]["html_url"])})
+
+		req = urllib.request.Request("%s/issues/%s/comments" % (dst_url, issue_number), comment)
+		req.add_header("Authorization", b"Basic " + base64.urlsafe_b64encode(username.encode("utf-8") + b":" + password.encode("utf-8")))
+		req.add_header("Content-Type", "application/json")
+		req.add_header("Accept", "application/json")
+		urllib.request.urlopen(req)
 
 def import_issues(issues, dst_milestones, dst_labels):
-	for source in issues:
-		labels = []
-		if source.has_key("labels"):
-			for src_label in source["labels"]:
-				name = src_label["name"]
-				for dst_label in dst_labels:
-					if dst_label["name"] == name:
-						labels.append(name)
-						break
+	for issue in issues:
 
-		milestone = None
-		if source.has_key("milestone") and source["milestone"] is not None:
-			title = source["milestone"]["title"]
-			for dst_milestone in dst_milestones:
-				if dst_milestone["title"] == title:
-					milestone = dst_milestone["number"]
-					break
+		issue_creator = issue["user"]["login"]
+		issue_url = issue["html_url"]
+		issue_id = issue["number"]
 
-		assignee = None
-		if source.has_key("assignee") and source["assignee"] is not None:
-			assignee = source["assignee"]["login"]
+		if "body" in issue and issue["body"] is not None:
+			issue_header = "Issue [#%s](%s) by: [%s](http://github.com/%s)\n" % (issue_id, issue_url, issue_creator, issue_creator)
+			issue["body"] = issue_header + "\n\n\n" + issue["body"]
 
-		body = None
-		if source.has_key("body") and source["body"] is not None:
-			body = source["body"]
-
-		dest = json.dumps({
-			"title": source["title"],
-		    "body": body,
-		    "assignee": assignee,
-		    "milestone": milestone,
-		    "labels": labels
-		})
-
-		comments = get_comments_on_issue(source)
-		#todo: insert logic on comments if needed
-
-		req = urllib2.Request("%s/issues" % dst_url, dest)
-		req.add_header("Authorization", "Basic " + base64.urlsafe_b64encode("%s:%s" % (username, password)))
+		req = urllib.request.Request("%s/issues" % dst_url, json.dumps(issue))
+		req.add_header("Authorization", b"Basic " + base64.urlsafe_b64encode(username.encode("utf-8") + b":" + password.encode("utf-8")))
 		req.add_header("Content-Type", "application/json")
 		req.add_header("Accept", "application/json")
-		res = urllib2.urlopen(req)
+		res = urllib.request.urlopen(req)
 
 		data = res.read()
-		res_issue = json.load(StringIO(data))
-		print "Successfully created issue %s" % res_issue["title"]
+		res_issue = json.loads(data.decode("utf-8"))
+
+		import_pull_requests(issue, res_issue["number"])
+
+		comments = get_comments_on_issue(issue)
+		import_comments(comments, res_issue["number"])
+
+		print("Successfully created issue %s" % res_issue["title"].encode("utf-8"))
 
 def main():
 	#get milestones and issues to import
