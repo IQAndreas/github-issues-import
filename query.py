b99a5fdd63b6e0239a1b0c426c236fb08e26b591
Leandro Lucarella <leandro.lucarella@sociomantic.com>
2014-12-30 20:00:13 +0000

    Don't ask questions when not in a tty
    This allows the program to be called via a pipe, for example to get
    a list of issues to copy from another command and then invoking the
    import via | xargs.

--- query.py
+++ query.py
@@ -13,6 +13,10 @@ def password(question):
 # Taken from http://code.activestate.com/recipes/577058-query-yesno/
 #  with some personal modifications
 def yes_no(question, default=True):
+    # Assume the default if we can't ask (not in a tty)
+    if not sys.stdin.isatty():
+        return default
+
     choices = {"yes":True, "y":True, "ye":True,
                "no":False, "n":False }
 
